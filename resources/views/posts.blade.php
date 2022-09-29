48eea23a161aaa0e798286f7862f06ddbf8b263e
toma-ohhdeer <tom.a@ohhdeer.com>
2022-09-29 15:07:13 +0100

    Update posts.blade.php

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -5,14 +5,7 @@
   <script src="/app.js"></script>
 </head>
 <body>
-
-<?php use Spatie\YamlFrontMatter\YamlFrontMatter;
-$files = collect(File::files(resource_path("posts")));?>
-<?php foreach ($files as $file): ?>
-<?=YamlFrontMatter::parseFile($file)->title?>
-<?php endforeach ?>
   <?php foreach ($posts as $post): ?>
-
     <article>
       <h1>
           <a href="/posts/<?= $post->slug;?>">
